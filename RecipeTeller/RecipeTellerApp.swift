9435b7aa0f230efca345ef2a26ce9594bdbd6105
Harsh Johar <hpsj3112@gmail.com>
2022-07-01 08:52:13 +0530

    Initial Commit

---
+++ RecipeTeller/RecipeTellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTellerApp.swift
+//  RecipeTeller
+//
+//  Created by Harsh Johar on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeTellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
